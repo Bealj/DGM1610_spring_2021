1622a23c17a9a393262b5e76aca95b397f445c49
Joram Beal <10923637@my.uvu.edu>
2021-03-17 16:55:32 -0600

    Added challenge 1

---
+++ Challenge 1/Assets/Challenge 1/Scripts/PropellerRotation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropellerRotation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Rotates the Propeller
+        transform.Rotate(Vector3.forward, 40);
+    }
+}
